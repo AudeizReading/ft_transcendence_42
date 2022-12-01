c87666c6656a96fe082d0e5ed1af89a4a04a9ff4
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-12-01 14:47:48 +0100

    Attend que PSQL accepte les connexions avant de lancer le back
    Ajout d'un message: `PSQL is not ready (pg_iready: $?), waiting 5 seconds.`
    Ca sera très beau en eval :)

--- app/pong/node-entrypoint.sh
+++ app/pong/node-entrypoint.sh
@@ -5,6 +5,21 @@ cd pong
 
 npm install
 
+(
+	set -a
+	source .env
+	set +a
+	until pg_isready --dbname=$PSQL_DATABASE --host=$PSQL_HOST --port=5432 --username=$PSQL_USER; do
+		EC=$?
+		if [ $EC -eq 127 ]; then
+			echo "command pg_isready doesn't exist, break until."
+			break
+		fi
+		echo "PSQL is not ready (pg_iready: $EC), waiting 5 seconds."
+		sleep 5
+	done
+)
+
 npx prisma migrate deploy
 
 npx prisma generate
