90fe10c1be40a008837173b8d39d231a48656f65
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-12-07 15:08:51 +0100

    Improve node-entrypoint.sh if pg_isready

--- app/pong/node-entrypoint.sh
+++ app/pong/node-entrypoint.sh
@@ -12,7 +12,9 @@ npm install
 	until pg_isready --dbname=$PSQL_DATABASE --host=$PSQL_HOST --port=5432 --username=$PSQL_USER; do
 		EC=$?
 		if [ $EC -eq 127 ]; then
-			echo "command pg_isready doesn't exist, break until."
+			echo "command pg_isready doesn't exist, try inside docker until."
+			DOCKER_CONTAINER_NAME="psql"
+			timeout 90s bash -c "until docker exec $DOCKER_CONTAINER_NAME pg_isready ; do sleep 5 ; done"
 			break
 		fi
 		echo "PSQL is not ready (pg_iready: $EC), waiting 5 seconds."
