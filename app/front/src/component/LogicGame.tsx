559db69666563b1644e473bb6a1ac6267cd74184
Guillaume <92152391+GlaceCoding@users.noreply.github.com>
2022-12-15 17:27:17 +0100

    support qwerty

--- app/front/src/component/LogicGame.tsx
+++ app/front/src/component/LogicGame.tsx
@@ -19,7 +19,6 @@ export function getPlayerPosition(player: dataPlayer) {
     const speed = player.speed * player.dir;
     //const delta = player.speed * player.dir * time;
     const delta = (1 / 2) * speed * (time * time);
-    console.log(delta);
     return Math.min(300 - player.size, Math.max(0, player.pos + delta));
   } else {
     return player.pos;
@@ -32,6 +31,7 @@ function LogicGame(props: {
   }) {
   const loaded = useRef(false);
   const keydowns = useRef({
+    '87': false,
     '90': false,
     '83': false,
     '38': false,
@@ -51,7 +51,7 @@ function LogicGame(props: {
     const handleKeyEvent = (event: KeyboardEvent) => {
       if (event.repeat)
         return ;
-      if (!(event.keyCode === 90 || event.keyCode === 83 // Z - S
+      if (!(event.keyCode === 87 || event.keyCode === 90 || event.keyCode === 83 // W - S
             || event.keyCode === 38 || event.keyCode === 40)) // arrow: ^ - v
         return ;
       if (event.type === 'keydown') {
@@ -62,14 +62,14 @@ function LogicGame(props: {
         keydowns.current[event.keyCode] = true;
         const player = props.data.players[0];
         player.pos = getPlayerPosition(player);
-        player.dir = (event.keyCode === 90 || event.keyCode === 38) ? -1 : +1; // if TOP -1 else +1
+        player.dir = (event.keyCode === 87 || event.keyCode === 90 || event.keyCode === 38) ? -1 : +1; // if TOP -1 else +1
         player.at = new Date();
       } else if (event.type === 'keyup') {
         keydowns.current[event.keyCode] = false;
         const player = props.data.players[0];
         let nextdir = 0;
         // TODO: Ajout d'une queue sur les touches enfoncées ? Parce que le dessus à toujours l'avantage sur la touche du bas
-        if (keydowns.current['90'] || keydowns.current['38'])
+        if (keydowns.current['87'] || keydowns.current['90'] || keydowns.current['38'])
           nextdir = -1;
         else if (keydowns.current['83'] || keydowns.current['40'])
           nextdir = 1;
@@ -84,6 +84,7 @@ function LogicGame(props: {
     };
 
     const handleFocus = (event: FocusEvent) => {
+      keydowns.current['87'] = false;
       keydowns.current['90'] = false;
       keydowns.current['83'] = false;
       keydowns.current['38'] = false;
