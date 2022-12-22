cd3e1a3f2b3fe9d7dfe9211f328b87607cbd483a
AudeizReading <alellouc@student.42nice.fr>
2022-12-22 17:18:42 +0100

    footer: component MatesList that keep 4 Mate components

---
+++ app/front/src/component/MatesList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Mate from '../component/Mate';
+
+function MatesList(props: any)
+{
+	const {mates} = props;
+	return (
+		<Box component="div" sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '35%', p: '0.15em', m: '0.25em'}}>
+			<Box component='span'>Powered By </Box>
+			{
+				mates.map(
+					(mate: any) => (<Mate key={mate.login} login={mate.login} avatar={mate.avatar}/>)
+				)
+			}
+		</Box>
+		);
+}
+
+export default MatesList;
