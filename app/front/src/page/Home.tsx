9ce583465b06a62b7380655f2c76af00f7eacbff
AudeizReading <alellouc@student.42nice.fr>
2022-12-22 12:57:41 +0100

    home: discard console log

--- app/front/src/page/Home.tsx
+++ app/front/src/page/Home.tsx
@@ -2,7 +2,6 @@ import Box from '@mui/material/Box';
 import Footer from './Footer';
 
 function Home() {
-      {console.log("ca marche je m'incruste");}
   return (
     <Box component="main" style={{ backgroundColor: "green", height:"400px", overflow:"hidden" }}>
       This is HOME &lt;3
