80c2a61f85246eda153b82e987852e87c5a89c6a
AudeizReading <alellouc@student.42nice.fr>
2022-12-22 12:55:13 +0100

    footer: flex-box, hei is now auto, adding mates props
    rather to do a component for mates props I guess, will try to play with 42 api for
    update style also

--- app/front/src/page/Footer.tsx
+++ app/front/src/page/Footer.tsx
@@ -1,17 +1,30 @@
+import React from 'react'
 import Box from '@mui/material/Box';
 
-function Footer()
+function Footer(props: any)
 {
 	const sx_footer = {
+		color: 'primary.main',
+		backgroundColor: 'white',
 		position: "fixed",
-		height: '40px',
+		height: 'auto',
+		width: '100%',
 		textAlign: "center",
-		mx: '50%'
+		m: 'auto',
+		p: 'auto',
+		display: 'flex',
+		flexFlow: 'row wrap',
+		justifyContent: 'center',
+		alignItems: 'stretch'
 	};
 
+console.log('On commence a jouer avec l\'api');
+
+//fetch();
 	return (
 		<Box component="footer" sx={sx_footer}>
-		Powered by someone.
-		</Box>);
+			Powered by {props.mate1}, {props.mate2}, {props.mate3}, {props.mate4}.
+		</Box>
+	);
 }
 export default Footer;
