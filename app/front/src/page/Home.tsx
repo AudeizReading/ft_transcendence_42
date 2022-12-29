73acc025236e8d77ddb7de48e25fc2d401a55e56
AudeizReading <alellouc@student.42nice.fr>
2022-12-29 12:38:34 +0100

    Home: update getting props from user directly
    Working also on the grid format, testing how it works

--- app/front/src/page/Home.tsx
+++ app/front/src/page/Home.tsx
@@ -10,20 +10,21 @@ function Home(props: {user: User}) {
 
   console.log(props.user);
 
-  const [isLogged, setIsLogged] = React.useState(props.user.connected);
+  const [user, setUser] = useState(props.user);
+  const [isLogged, setIsLogged] = useState(user.connected);
   
   // j'aimerais que ca change le isLogged si l'user s'est connecté
   useEffect(() => {
-    if (props.user.connected === true)
+    if (user.connected === true)
       setIsLogged(!isLogged);
-  }, [props.user.connected]);
+  }, [user]);
 
   return (
     <Box component="main" style={{ backgroundColor: "green", height:"400px", overflow:"hidden" }}>
       <Grid container >
-        <Grid item xs={6} sm={6} md={2} lg={2} xl={2}>Premier item de grille</Grid>
-        <Grid item xs={12} sm={12} md={7} lg={7} xl={7}>Deuxieme item de grille: {isLogged === false ? <Clock/> : "You are logged ans your dashboard should be displayed"}</Grid>
-        <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>Troisieme item de grille</Grid>
+        <Grid item xs={6} sm={12} md={6} lg={3} xl={3}>Premier item de grille</Grid>
+        <Grid item xs={12} sm={12} md={6} lg={7} xl={7}>Deuxieme item de grille: {isLogged === false ? <Clock/> : "You are logged ans your dashboard should be displayed"}</Grid>
+        <Grid item xs={6} sm={12} md={12} lg={2} xl={2}>Troisieme item de grille</Grid>
       </Grid>
       This is HOME &lt;3
       <Footer />`
