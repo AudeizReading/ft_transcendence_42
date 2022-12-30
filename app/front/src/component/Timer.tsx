c871ff99524c4fb0a6bd1359da2c89f2d22b8a11
AudeizReading <alellouc@student.42nice.fr>
2022-12-30 23:59:35 +0100

    Timer: fix displaying
    forgot to modulo the values for a coherent displaying hh:mm:ss
    need to be stylished and decide what to do with it (TimerProvider?)

--- app/front/src/component/Timer.tsx
+++ app/front/src/component/Timer.tsx
@@ -1,18 +1,33 @@
 import Box from '@mui/material/Box'
 import useInterval from '../hooks/useInterval'
 import formattingNumber from '../dep/formattingNumber';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 function Timer()
 {
   const [count, setCount] = useState(0);
+  const [seconds, setSeconds] = useState(count % 60);
+  const [minutes, setMinutes] = useState(Math.floor(count / 60));
+  const [hours, setHours] = useState(Math.floor(count / 3600));
 
   useInterval(() => {
     setCount(count + 1);
   }, 1000);
 
+  useEffect(() => setSeconds(count % 60), [count]);
+  useEffect(() => {seconds === 0 && setMinutes(Math.floor((count / 60) % 60))}, [seconds]);
+  useEffect(() => {minutes === 0 && setHours(Math.floor((count / 3600) % 3600))}, [minutes]);
+
   return (
-    <Box component="span">{formattingNumber(count)} seconds are elapsed since </Box>);
+    <Box component="div">
+      <Box component="span">{formattingNumber(count)}</Box>
+      <Box component="div">
+        <Box component="span">{formattingNumber(hours)}:</Box>
+        <Box component="span">{formattingNumber(minutes)}:</Box>
+        <Box component="span">{formattingNumber(seconds)}</Box>
+      </Box>
+    </Box>
+    );
 }
 
 export default Timer;
