c1f54779c2217e5ffe74832767813331c52fd16c
AudeizReading <alellouc@student.42nice.fr>
2023-01-10 19:27:48 +0100

    Home: Update BoxPaper:
    J'ai routé les Box a leurs themes, il y a juste pour options ou je ne sais pas vers quoi router
    
    J'ai remplacé SOCIAL par AMIS, je trouve ca plus chaleureux
    
    J'ai rempli les bground des boxs avec des images comme fait pour la box JOUER, j'ai regrouper les datas dans un array d'objs pour boucler dessus et n'avoir besoin que d'un composant qui sera reproduit a chaque iteration du tableau
    Je me suis laissée guider par adobe kuler image pour chopper le style qui colle au bg que je pose ci et la pour tester (font blanche obligatoire pour le contraste)
    L'idee c'est d'avoir les box en colonnes en mode petit et en ligne en mode medium si il n'y a aucune data a recup, le tout centré sur ses axes.
    
    Si il y a des datas en bdd, les boxs iront sur la gauche et le reste de la droite devrait etre occupé par le dashboard.
    Pour le moment c'est juste flexboxé

--- app/front/src/page/Home.tsx
+++ app/front/src/page/Home.tsx
@@ -15,12 +15,16 @@ import AnalogicClock from '../component/AnalogicClock';
 
 import UnstableGrid from '@mui/system/Unstable_Grid';
 
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
 const BoxPaper = styled(Paper)(({ theme }) => ({
   backgroundColor: 'rgba(0,0,0,0.5)',
   color: 'white',
+  textShadow: '1px -2px 3px #3F528C, 0 0 5px #3F528C',
   minWidth: '14vw',
   minHeight: '14vw',
   maxWidth: '14vw',
@@ -79,7 +83,7 @@ function Home(props: {
         <UnstableGrid sx={{position: 'relative', width: '100%', mx: '2%', p: '1%'}}>
           <UnstableGrid xs={12} md={5} mdOffset={7} sx={{position: 'relative', display: 'flex', flexFlow: 'column', alignItems: 'center'}}>
             <AnalogicClock stress={true} time={time}/>
-            <UnstableGrid xs={12} mdOffset={0} sx={{position: 'absolute', top: '62%', width: '100%', textAlign: 'center', zIndex: 10, color: 'black' }}>
+            <UnstableGrid xs={12} mdOffset={0} sx={{position: 'absolute', top: '62%', width: '100%', textAlign: 'center', zIndex: 10, color: '#024959' }}>
               <Clock component="h6" time={time}/>
             </UnstableGrid>
           </UnstableGrid>
@@ -93,30 +97,113 @@ function Home(props: {
       </UnstableGrid>
   );
 
+  const datasBoxPaper = [
+    {
+      sx: {
+        background: 'url(/res/pong/iceberg_full.png), url(https://www.desktopbackground.org/download/1152x864/2014/04/15/747399_ocean-water-night-backgrounds_1255x1024_h.jpg)',
+        backgroundPosition: 'center 80%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%, cover',
+      },
+      child: {sx: {},},
+      name:'JOUER',
+      url: '/play',
+    },
+    {
+      sx: 
+      {
+        background: 'url(https://www.desktopbackground.org/p/2015/09/06/1007039_aurora-borealis-iphone-wallpapers-wallpaper_1920x1200_h.jpg)',
+        backgroundPosition: 'center 80%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        transform: 'rotate3d(0, 1, 0, 180deg)',
+      },
+      child: {
+        sx: {
+          transform: 'rotate3d(0, 1, 0, 180deg)',
+        },
+      },
+      name:'SCORE',
+      url: '/score',
+    },
+    {
+      sx: {
+        background: 'url(https://www.desktopbackground.org/p/2014/03/04/726085_penguin-wallpapers-1280x800-wallpapers-penguin-1280x800-wallpapers_1280x800_h.jpg)',
+        backgroundPosition: 'center 80%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      },
+      child: {sx:{},},
+      name: 'AMIS',
+      url: '/friend',
+    },
+    {
+      sx: {
+        background: 'url(https://www.desktopbackground.org/download/o/2011/08/14/250058_aurora-borealis-from-space-wallpaper_1920x1080_h.jpg)',
+        backgroundPosition: 'center 80%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      },
+      child: {sx:{},},
+      name: 'OPTIONS',
+      url: '/',
+    },
+  ];
+
+  const LinkBox = (
+      datasBoxPaper.map((data) =>
+        {
+          return (
+          <BoxPaper sx={data.sx}>
+                <Box component="p" sx={data.child.sx}><Link to={data.url} component={RouterLink} color="inherit" underline="none">{data.name}</Link></Box>
+          </BoxPaper>
+            
+          )
+        }
+  ));
+
   return (
     <Box component="main" sx={{ height: '100vh', overflow: 'auto' }}>
       {isLogged === false ?
         <Fade in={true} timeout={400}>{gridNotLogged}</Fade>
         :
         <React.Fragment>
-          <Box sx={{ display: 'flex', mt: '10vh', mx: 10, gap: '2vw' }}>
-            <BoxPaper sx={{
-              background: 'url(/res/pong/iceberg_full.png), url(https://www.desktopbackground.org/download/1152x864/2014/04/15/747399_ocean-water-night-backgrounds_1255x1024_h.jpg)',
-              backgroundPosition: 'center 80%',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: '100%, cover',
-            }}>
-              <Box component="p">JOUER</Box>
-            </BoxPaper>
-            <BoxPaper>
-              <Box component="p">SCORE</Box>
-            </BoxPaper>
-            <BoxPaper>
-              <Box component="p">SOCIAL</Box>
-            </BoxPaper>
-            <BoxPaper>
-              <Box component="p">OPTION</Box>
-            </BoxPaper>
+        <Box sx={{
+          display: 'flex', 
+          flexDirection: {
+            xs: 'row', 
+            md: 'column',
+          }
+          }}>
+        <Box sx={{ 
+          display: 'flex', 
+          border: '2px solid #027368', 
+          flexDirection: {
+            xs: 'column', 
+            md: 'row',
+          },
+          justifyContent: {
+            xs:'flex-start',
+            md: 'center',
+          },
+          minWidth: '14vw',
+          minHeight: '14vw',
+          maxWidth: {xs: '14vw', md: '100%'},
+          maxHeight: {xs: '100%', md: '14vw'},
+          mt: '10vh', 
+          mx: 10, 
+          gap: {
+            xs: '2vw', 
+            lg: '2vw'
+          } 
+        }}>
+              
+            {
+              LinkBox
+            }
+           
+          </Box>
+          <Box component="div"sx={{ width: 200, display: 'flex', flexDirection: 'row', border: '2px solid #3F528C'}}>Futur Dashboard</Box>
           </Box>
         </React.Fragment>}
       {isLogged === false && <Footer />}
